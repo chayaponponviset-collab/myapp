9b7828891f0991e365f553ab9a3265de9fe2a708
chayaponponviset <chayaponponviset@gmail.com>
2025-12-02 05:05:03 +0000

    Recipe List Design

---
+++ lib/model/recipe.dart
@@ -0,0 +1,14 @@
+class Recipe {
+  String imgUrl;
+  String imgTitle;
+
+  Recipe(this.imgUrl, this.imgTitle);
+
+   static List<Recipe> samples = [
+   Recipe('assets/images/200304_Olive_PadThai-ebe0f22-scaled.webp','PadThai',),
+   Recipe('assets/images/3910.webp','Sweet Thai basil smash cocktail',),
+   Recipe('assets/images/OLI-0519-Healthy_GreenThaiFishCurry_32208-c3cee9a.webp','GreenThaiFishCurry',),
+   Recipe('assets/images/p110-20250121rps2352pumpkincurrypreview-8534543.webp','pumpkincurry',),
+  ];
+
+}
