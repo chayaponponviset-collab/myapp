d2e123d4f9590fa45766960cf0775b35e4fe6f7a
chayaponponviset <chayaponponviset@gmail.com>
2025-12-16 05:04:19 +0000

    Recipe ingredient

--- lib/model/recipe.dart
+++ lib/model/recipe.dart
@@ -1,15 +1,69 @@
+import 'package:myapp/model/Ingredient.dart';
+
 class Recipe {
   String imgUrl;
   String imgTitle;
   String description;
+  List<Ingredient> ingredients;
 
-  Recipe(this.imgUrl, this.imgTitle , this.description);
+  Recipe(this.imgUrl, this.imgTitle, this.description, this.ingredients);
 
-   static List<Recipe> samples = [
-   Recipe('assets/images/200304_Olive_PadThai-ebe0f22-scaled.webp','PadThai', 'A classic and iconic Thai street food dish made with stir-fried rice noodles, shrimp (or chicken/tofu), peanuts, scrambled egg, and bean sprouts, all coated in a tangy, sweet, and savory sauce'),
-   Recipe('assets/images/3910.webp','Sweet Thai basil smash cocktail', 'A vibrant and aromatic cocktail featuring the peppery-sweet notes of fresh Thai basil, muddled with a splash of citrus and balanced by a touch of sweetness, typically served with a base spirit like gin or vodka'),
-   Recipe('assets/images/OLI-0519-Healthy_GreenThaiFishCurry_32208-c3cee9a.webp','GreenThaiFishCurry', 'A fragrant and flavorful Thai curry featuring tender white fish simmered in a rich, creamy sauce made from green curry paste, coconut milk, and aromatic Thai ingredients like kaffir lime leaves and Thai basil'),
-   Recipe('assets/images/p110-20250121rps2352pumpkincurrypreview-8534543.webp','pumpkincurry', 'A comforting and hearty vegetarian curry featuring sweet, cubed pumpkin simmered in a rich, spiced coconut milk base. Often enriched with ingredients like ginger, red chilies, and spinach for a savory-sweet depth'),
+  static List<Recipe> samples = [
+    Recipe(
+      'assets/images/200304_Olive_PadThai-ebe0f22-scaled.webp',
+      'PadThai',
+      'A classic and iconic Thai street food dish made with stir-fried rice noodles, shrimp (or chicken/tofu), peanuts, scrambled egg, and bean sprouts, all coated in a tangy, sweet, and savory sauce',
+      [
+        Ingredient(name: 'Rice noodles (Pad Thai noodles)',quantity: 200,unit: 'g',),
+        Ingredient(name: 'Shrimp', quantity: 150, unit: 'g'),
+        Ingredient(name: 'Firm tofu', quantity: 100, unit: 'g'),
+        Ingredient(name: 'Eggs', quantity: 2, unit: ''),
+        Ingredient(name: 'Bean sprouts', quantity: 1, unit: 'cup'),
+        Ingredient(name: 'Garlic chives', quantity: 1, unit: 'cup'),
+
+      ],
+    ),
+
+    Recipe(
+      'assets/images/3910.webp',
+      'Sweet Thai basil smash cocktail',
+      'A vibrant and aromatic cocktail featuring the peppery-sweet notes of fresh Thai basil, muddled with a splash of citrus and balanced by a touch of sweetness, typically served with a base spirit like gin or vodka',
+      [
+        Ingredient(name: 'Thai basil leaves', quantity: 1, unit: 'handful'),
+        Ingredient(name: 'Lime', quantity: 1, unit: ''),
+        Ingredient(name: 'Simple syrup', quantity: 20, unit: 'ml'),
+        Ingredient(name: 'White rum', quantity: 50, unit: 'ml'),
+        Ingredient(name: 'Soda water', quantity: 60, unit: 'ml'),
+        Ingredient(name: 'Ice cubes', quantity: 1, unit: 'cup'),
+      ],
+    ),
+
+    Recipe(
+      'assets/images/OLI-0519-Healthy_GreenThaiFishCurry_32208-c3cee9a.webp',
+      'GreenThaiFishCurry',
+      'A fragrant and flavorful Thai curry featuring tender white fish simmered in a rich, creamy sauce made from green curry paste, coconut milk, and aromatic Thai ingredients like kaffir lime leaves and Thai basil',
+      [
+        Ingredient(name: 'White fish fillet', quantity: 400, unit: 'g'),
+        Ingredient(name: 'Green curry paste', quantity: 2, unit: 'tbsp'),
+        Ingredient(name: 'Coconut milk', quantity: 2, unit: 'cups'),
+        Ingredient(name: 'Vegetable stock', quantity: 1, unit: 'cup'),
+        Ingredient(name: 'Thai eggplants', quantity: 4, unit: ''),
+        Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+      ],
+    ),
+
+    Recipe(
+      'assets/images/p110-20250121rps2352pumpkincurrypreview-8534543.webp',
+      'pumpkincurry',
+      'A comforting and hearty vegetarian curry featuring sweet, cubed pumpkin simmered in a rich, spiced coconut milk base. Often enriched with ingredients like ginger, red chilies, and spinach for a savory-sweet depth',
+      [
+        Ingredient(name: 'Pumpkin', quantity: 400, unit: 'g'),
+        Ingredient(name: 'Red curry paste', quantity: 2, unit: 'tbsp'),
+        Ingredient(name: 'Coconut milk', quantity: 2, unit: 'cups'),
+        Ingredient(name: 'Vegetable stock', quantity: 1, unit: 'cup'),
+        Ingredient(name: 'Palm sugar', quantity: 1, unit: 'tbsp'),
+        Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+      ],
+    ),
   ];
-
 }
--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -1,28 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/model/recipe.dart';
 
-class RecipeDetail extends StatelessWidget {
+class RecipeDetail extends StatefulWidget {
   // receive data from main.dart
   final Recipe recipe;
 
   const RecipeDetail({super.key, required this.recipe});
 
   @override
+  State<RecipeDetail> createState() => _RecipeDetailState();
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  double sliderValue = 1;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Recipe Detail')),
-      body: Column(
-        children: [
-          Image.asset(
-            recipe.imgUrl,
-            height: 250,
-            width: double.infinity,
-            fit: BoxFit.cover,
+      appBar: AppBar(title: Text(widget.recipe.imgTitle)),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                widget.recipe.imgUrl,
+                height: 100,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                widget.recipe.imgTitle,
+                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                widget.recipe.description,
+                style: const TextStyle(
+                  fontFamily: 'BerkshireSwash',
+                  fontSize: 20.0,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    final ingredient = widget.recipe.ingredients[index];
+                    return Text(
+                      '${(ingredient.quantity * sliderValue).toStringAsFixed(1)} ${ingredient.unit} ${ingredient.name}',
+                      style: const TextStyle(fontSize: 16.0),
+                    );
+                  },
+                  itemCount: widget.recipe.ingredients.length,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text('Servings: ${sliderValue.round()}',
+                  style: Theme.of(context).textTheme.titleMedium),
+              Slider(
+                min: 1,
+                max: 5,
+                divisions: 4,
+                value: sliderValue,
+                label: sliderValue.round().toString(),
+                onChanged: (newValue) {
+                  setState(() {
+                    sliderValue = newValue;
+                  });
+                },
+                activeColor: Colors.green,
+                inactiveColor: Colors.grey[300],
+              ),
+            ],
           ),
-          SizedBox(height: 16.0),
-          Text(recipe.imgTitle, style: TextStyle(color: Colors.green, fontSize: 20 , fontWeight: FontWeight.bold),),
-          Text(recipe.description, style: TextStyle(fontFamily: 'BerkshireSwash', fontSize: 20.0 , backgroundColor: Colors.yellow),),
-        ],
+        ),
       ),
     );
   }
